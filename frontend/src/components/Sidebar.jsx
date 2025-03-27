f5dfe26ef41dc22dd0c75f06b95f2c3b26f5cd7e
Max Kly <114592559+maxklymenko@users.noreply.github.com>
2025-03-27 18:34:16 +0000

    Add basic sidebar UI

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,5 +1,8 @@
 import { Link } from "react-router"
 import HomeIcon from "../icons/HomeIcon"
+import MessageIcon from "../icons/MessageIcon"
+import PeopleIcon from "../icons/PeopleIcon"
+import SidebarCommunities from "./SidebarCommunities"
 const Sidebar = () => {
     return (
         <div className="sidebar">
@@ -8,6 +11,18 @@ const Sidebar = () => {
                     <HomeIcon />
                     <p>Feed</p>
                 </Link>
+                <Link to='messages' className="nav">
+                    <MessageIcon />
+                    <p>Messages</p>
+                </Link>
+                <Link to='people' className="nav">
+                    <PeopleIcon />
+                    <p>People</p>
+                </Link>
+            </div>
+            <div className="community-navs">
+                <input type="text" placeholder="🔍  Look for communities" />
+                <SidebarCommunities />
             </div>
         </div>
     )
